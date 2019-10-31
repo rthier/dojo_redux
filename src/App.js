05626671ee51f3e4a58905ae6612fc77d36d63d0
Romenigue Thier <romenigue.thier@gmail.com>
2019-10-31 18:33:24 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  render() {
+   return ( <div className="App">
+      <ul>
+        {this.props.todoList.map( (todo) => 
+          (
+            <li>
+              <span>{todo.title}</span>
+              <input type="checkbox" checked={todo.done}></input>
+           </li>
+          )
+        )    
+        }
+      </ul>
     </div>
-  );
+  )
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  todos = state.todos
+}
+
+const mapDispatchToProps = dispatch => {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,53 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const todoList = [
+    {
+        id: 1,
+        title: "commitar",
+        done: true,
+    },
+    {
+        id: 2,
+        title: "criar mr",
+        done: true,
+    },
+    {
+        id: 3,
+        title: "teste de integracao no pipeline",
+        done: false,
+    },
+    {
+        id: 4,
+        title: "impeachment",
+        done: false,
+    }
+]
+
+
+
+function todosReducer(state = [], action) {
+  switch (action.type) {
+    case 'LOAD_TODOS':
+      return {
+          todos: todoList
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(todosReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
